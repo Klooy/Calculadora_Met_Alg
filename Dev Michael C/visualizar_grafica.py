a1cf08c1092df2f75a8eaec3ef0ae80459287a7e
Klooy <92549161+Klooy@users.noreply.github.com>
2023-11-21 17:46:23 -0500

    Aportes funcionalidad completa Michael C.
    funcionalidad completa del sistema para hallar la raíz aproximada de una función con 4 métodos diferentes  y sus respectivas graficas, así como también una funcionalidad extra para elegir el mejor método para cada función (comparar métodos).

---
+++ Dev Michael C/visualizar_grafica.py
@@ -0,0 +1,76 @@
+#SCRIPT PARA VISUALIZAR LA GRAFICA DE CADA METODO
+#DEV MICHAEL FELIPE CORRALES FLOREZ - 1007423576
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# Función para visualizar la gráfica
+def visualizar_grafica(funcion_str):
+    # Convertir la cadena de texto en una función
+    funcion = eval("lambda x: " + funcion_str)
+
+    # Vectorizar la función para que pueda tomar un array numpy
+    vf = np.vectorize(funcion)
+
+    # Crear un array de valores x
+    x = np.linspace(-5, 5, 400)
+
+    # Calcular los valores y
+    y = vf(x)
+
+    # Crear el gráfico
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(x, y, label='f(x) = ' + funcion_str)
+
+    # Establecer los límites de los ejes
+    ax.set_xlim([-5, 5])
+    ax.set_ylim([-5, 5])
+
+    # Establecer la cuadrícula
+    ax.grid(True)
+    ax.set_xticks(np.arange(-5, 6, 0.5))
+    ax.set_yticks(np.arange(-5, 6, 0.5))
+
+    # Dibujar el plano cartesiano
+    ax.axhline(0, color='black',linewidth=0.5)
+    ax.axvline(0, color='black',linewidth=0.5)
+
+    # Configurar las etiquetas y el título
+    ax.set_xlabel('x', fontsize=14, fontweight='bold')
+    ax.set_ylabel('y', fontsize=14, fontweight='bold')
+    ax.set_title('Gráfico de la función f(x)', fontsize=16, fontweight='bold')
+
+    ax.legend()
+
+    return fig
+
+def iniciar_ventana():
+    # Crear la ventana de tkinter
+    window = tk.Tk()
+
+    # Crear un widget de entrada de texto
+    entry = tk.Entry(window)
+    entry.pack()
+
+    # Función para manejar el evento del botón
+    def on_button_click():
+        # Obtener la función ingresada por el usuario
+        funcion_str = entry.get()
+
+        # Generar la gráfica
+        fig = visualizar_grafica(funcion_str)
+
+        # Crear un canvas para mostrar la gráfica en la ventana de tkinter
+        canvas = FigureCanvasTkAgg(fig, master=window)
+        canvas.draw()
+        canvas.get_tk_widget().pack()
+
+    # Crear un botón que llame a la función on_button_click cuando se presione
+    button = tk.Button(window, text="Generar gráfica", command=on_button_click)
+    button.pack()
+
+    # Iniciar la ventana de tkinter
+    window.mainloop()
